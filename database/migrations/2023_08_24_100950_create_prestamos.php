83860e7cb6e5d9cb6b13237cda30271c00ef680a
Ruben Vargas <rubenvargasrodriguez4@gmail.com>
2023-08-24 12:29:34 +0200

    migrations succesfull

---
+++ database/migrations/2023_08_24_100950_create_prestamos.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('prestamos', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id', false, true);
+            $table->integer('book_id', false, true);
+            $table->date('fecha_devolucion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('prestamos');
+    }
+};
